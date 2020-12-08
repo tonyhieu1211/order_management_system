fc14371375ea3088ecc7c1e009a440e6196635a3
tran minh hieu <tranminhhieu9901@gmail.com>
2020-12-08 15:14:50 +0700

    file handle ajax edit profile request

--- edit_profile.php
+++ edit_profile.php
@@ -1,1 +1,28 @@
 <?php
+    include('database_connection.php');
+    if(isset($_POST["user_name"])){
+       if($_POST["user_new_password"] != ''){
+           $query = "
+           UPDATE user SET
+           name = '".$_POST["user_name"]."',
+           email = '".$_POST["user_email"]."',
+           password = '".password_hash($_POST["user_new_password"],PASSWORD_DEFAULT)."'
+           WHERE id = '".$_SESSION["user_id"]."'
+           ";
+       } else {
+           $query = "
+           UPDATE user SET
+           name = '".$_POST["user_name"]."',
+           email = '".$_POST["user_email"]."'
+           WHERE id = '".$_SESSION["user_id"]."'
+           ";
+       }
+       $statement = $connect->prepare($query);
+       $statement->execute();
+       $result = $statement->fetchAll();
+       if(isset($result)){
+           echo '<div class="alert alert-success">Hồ sơ đã được sửa</div>';
+           $_SESSION["user_name"] = $_POST["user_name"];
+       }
+    }
+?>
